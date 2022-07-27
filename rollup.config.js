9dab510628066a2f4316bfe3649559e39b10480e
rookior <1757629766@qq.com>
2022-07-27 17:44:46 +0800

    修改roolup打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,11 @@ import babel from '@rollup/plugin-babel';
 export default {
     input: 'src/index.js',
     output: {
-        file: 'dist/openioe.js'
+        file: 'dist/openioe.js',
+        // format: 'cjs',  //默认值
+        //当入口文件有export时，'umd'格式必须指定name。这样，在通过<script>标签引入时，才能通过name访问到export的内容
+         format: 'umd',
+         name: 'openioe'
     },
     plugins: [resolve(), babel({ babelHelpers: 'bundled' })]
 };
